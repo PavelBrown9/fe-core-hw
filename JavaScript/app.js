30f53444c4a65d696fa85c54ac7e80c5ce85455f
Pavlo <68113046+PavelBrown9@users.noreply.github.com>
2021-10-21 15:58:07 +0300

    Update app.js

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -87,7 +87,7 @@ const cloneH2 = h1.cloneNode(false);
 
 //add attr
 imgContainer.setAttribute('id', 'main-image-container');
-mainImg.setAttribute('src', 'http://s3.foxfilm.com/foxmovies/production/films/103/images/gallery/deadpool1-gallery-image.jpg');
+mainImg.setAttribute('src', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwallpaper.dog%2Fdeadpool-heart-wallpapers&psig=AOvVaw2NCGKrKyJDBC2XlnRqq9qf&ust=1634907423053000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJC67vLG2_MCFQAAAAAdAAAAABAJ');
 mainImg.setAttribute('alt', 'picture');
 imgContainer1.setAttribute('id', 'main-image-container');
 mainImg1.setAttribute('src', 'https://lumiere-a.akamaihd.net/v1/images/image_299ffd51.jpeg?region=0%2C0%2C1800%2C968');
